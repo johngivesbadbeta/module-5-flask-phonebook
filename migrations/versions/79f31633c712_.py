116698604068cfe49e6cbb7ac1c592740797a7f0
John Pinano <jpinano@gmail.com>
2023-05-03 10:25:18 -0700

    first commit

---
+++ migrations/versions/79f31633c712_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 79f31633c712
+Revises: 
+Create Date: 2023-05-01 09:34:30.201749
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '79f31633c712'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
